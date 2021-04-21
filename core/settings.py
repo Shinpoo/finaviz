c0b643d0c4ee2f7c65bd1d725099295913ca4ad5
Sélim El Mekki <selim.elmekki@student.uliege.be>
2021-04-21 23:54:44 +0200

    env

--- core/settings.py
+++ core/settings.py
@@ -17,10 +17,12 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config(
-    'SERVER', default='127.0.0.1'), 'finaviz.herokuapp.com']
+    'SERVER', default='127.0.0.1')]#, 'finaviz.herokuapp.com']
 
 # Application definition
 
