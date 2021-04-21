24d3c8b715732d1203f0c3a3c019fd58d6b84338
Sélim El Mekki <selim.elmekki@student.uliege.be>
2021-04-21 03:16:04 +0200

    procfile

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +130,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+# Extra places for collectstatic  to find static files.
 STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'core/static'),
 )
