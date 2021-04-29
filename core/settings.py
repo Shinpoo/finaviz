d142d044e02ccbd74db0b292e626709618f5f365
Sélim El Mekki <selim.elmekki@student.uliege.be>
2021-04-29 20:20:21 +0200

    added config for eu db

--- core/settings.py
+++ core/settings.py
@@ -97,13 +97,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'finaviz_db', #check pgadmin
+#         'USER': 'echkin',
+#         'PASSWORD': 'Chaosrain123-',
+#         'HOST': 'database-1.caivapzwfqye.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'finaviz_db', #check pgadmin
+        'NAME': 'finaviz_db',  # check pgadmin
         'USER': 'echkin',
         'PASSWORD': 'Chaosrain123-',
-        'HOST': 'database-1.caivapzwfqye.us-east-2.rds.amazonaws.com',
+        'HOST': 'database-1.cxqxaj0ucdvc.eu-central-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
